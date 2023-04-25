4ba55120bf27a6610c7e6fe7ee5bda08ed481206
Piceratop Ultimate <77230280+MawatariTanaka@users.noreply.github.com>
2023-04-25 10:37:22 +0700

    update equalidle/scripts/game.js

--- equalidle/scripts/game.js
+++ equalidle/scripts/game.js
@@ -8,7 +8,7 @@ function addRowSquares(component) {
   for (let i = 0; i < numSquaresPerRow; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.style.delayAnimation = `${delayAnimation * (i + 1)}s`
+    square.style.['animationDelay'] = `${delayAnimation * (i + 1)}s`
     const blankSide = document.createElement("div");
     blankSide.classList.add("blank-side");
     const digitSide = document.createElement("div");
