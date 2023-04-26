d1a6b8a98e6f822defc7d0c9a54a20f0d3c71986
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-04-26 18:23:26 +0700

    Read equations from the file

--- equalidle/scripts/game.js
+++ equalidle/scripts/game.js
@@ -72,7 +72,7 @@ for (
   }
 }
 
-// Set up problems
+// All valid characters
 
 const operators = ["+", "-", "*", "/"];
 const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -84,210 +84,195 @@ const keyEquivalents = [
   ["/", "÷"],
 ];
 
+let lockedPad = true;
+
 async function fetchEquation() {
-const getEquationFile = await fetch("./equation/fiveCharacterEquations.txt");
-await console.log("fetched")
-const equationFile = await getEquationFile.text();
-
-console.log(equationFile);
+  const getEquationFile = await fetch(
+    "scripts/equation/fiveCharacterEquations.txt"
+  );
+  const equationFile = await getEquationFile.text();
+  lockedPad = false;
+  return equationFile.split("\n");
 }
 
-fetchEquation();
+async function main() {
+  // Set up problems
 
-
-let counters = [0, 0, 0, 0];
-const allValidEqualities = [];
-
-while (counters[0] < digits.length) {
-  let left_side = digits[counters[0]];
-  let right_side =
-    digits[counters[1]] + operators[counters[2]] + digits[counters[3]];
-  let digitCounterSet = new Set([0, 1, 3]);
-  try {
-    if (eval(left_side) == eval(right_side)) {
-      allValidEqualities.push(left_side + "=" + right_side);
-      allValidEqualities.push(right_side + "=" + left_side);
-    }
-  } catch {}
-  for (let i = counters.length - 1; i >= 0; i--) {
-    counters[i]++;
-    if (
-      (digitCounterSet.has(i) && counters[i] < digits.length) ||
-      (i == 2 && counters[i] < operators.length)
-    ) {
-      break;
-    }
-    if (i) {
-      counters[i] = 0;
-    }
-  }
-}
-
-let hiddenEquality =
-  allValidEqualities[Math.floor(Math.random() * allValidEqualities.length)];
-for (let i = 0; i < keyEquivalents.length; i++) {
-  const charToReplace = keyEquivalents[i][0];
-  const replacementChar = keyEquivalents[i][1];
-  hiddenEquality = hiddenEquality.split(charToReplace).join(replacementChar);
-}
-
-// Setup answer validator
-
-let inputEquality = "";
-
-function checkValidAnswer(str) {
+  const allValidEqualities = await fetchEquation();
+  let hiddenEquality =
+    allValidEqualities[Math.floor(Math.random() * allValidEqualities.length)];
   for (let i = 0; i < keyEquivalents.length; i++) {
-    const charToReplace = keyEquivalents[i][1];
-    const replacementChar = keyEquivalents[i][0];
-    str = str.split(charToReplace).join(replacementChar);
+    const charToReplace = keyEquivalents[i][0];
+    const replacementChar = keyEquivalents[i][1];
+    hiddenEquality = hiddenEquality.split(charToReplace).join(replacementChar);
   }
-  if (!str.includes("=")) {
-    return false;
-  }
-  const left_side = str.slice(0, str.indexOf("="));
-  const right_side = str.slice(str.indexOf("=") + 1);
-  try {
-    return eval(left_side) == eval(right_side);
-  } catch {}
-  return false;
-}
 
-let finished = false;
-let lockedPad = false;
+  console.log(hiddenEquality);
 
-function updateUserAnswer(key) {
-  const inputRowSquares = Array.from(
-    gridContainer.querySelectorAll(".square")
-  ).slice(-numSquaresPerRow);
-  function modifyInputSquare() {
-    inputRowSquares.forEach((square, i) => {
-      const blankSide = square.querySelector(".blank-side");
-      const digitSide = square.querySelector(".digit-side");
-      if (i < inputEquality.length) {
-        blankSide.innerHTML = inputEquality[i];
-        digitSide.innerHTML = inputEquality[i];
-      } else {
-        blankSide.innerHTML = "";
-        digitSide.innerHTML = "";
+  // Setup answer validator
+
+  let inputEquality = "";
+
+  function checkValidAnswer(inputEquation) {
+    for (let i = 0; i < keyEquivalents.length; i++) {
+      const charToReplace = keyEquivalents[i][1];
+      const replacementChar = keyEquivalents[i][0];
+      inputEquation = inputEquation.split(charToReplace).join(replacementChar);
+    }
+    if (!inputEquation.includes("=")) {
+      return false;
+    }
+    const expressions = inputEquation.split("=").map((expr) => eval(expr));
+    for (let i = 0; i < expressions.length - 1; i++) {
+      if (expressions[i] !== expressions[i + 1]) {
+        return false;
       }
-    });
+    }
+    return true;
   }
-  switch (key) {
-    case "Backspace":
-    case "Delete":
-    case "Del":
-      if (inputEquality.length > 0) {
-        inputEquality = inputEquality.slice(0, -1);
-      }
-      modifyInputSquare();
-      break;
-    case "Enter":
-      if (checkValidAnswer(inputEquality) && inputEquality.length == 5) {
-        lockedPad = true;
-        // Handle color Logic
-        const colorCode = [
-          "darkred",
-          "darkred",
-          "darkred",
-          "darkred",
-          "darkred",
-        ];
-        const secondPassInput = [];
-        const secondPassHidden = [];
-        inputEquality.split("").forEach((char, i) => {
-          if (char == hiddenEquality[i]) {
-            colorCode[i] = "darkgreen";
-          } else {
-            secondPassInput.push([i, char]);
-            secondPassHidden.push(hiddenEquality[i]);
-          }
-        });
-        secondPassInput.forEach((input, i) => {
-          if (secondPassHidden.includes(input[1])) {
-            colorCode[input[0]] = "goldenrod";
-            secondPassHidden.splice(secondPassHidden.indexOf(input[1]), 1);
-          }
-        });
-        for (let i = 0; i < numSquaresPerRow; i++) {
-          const square = inputRowSquares[i];
-          const digitSide = square.querySelector(".digit-side");
-          digitSide.style.backgroundColor = colorCode[i];
-          setTimeout(() => {
-            square.classList.add("flipped");
-          }, flipDelayTime * i * 500);
-        }
-        if (
-          colorCode.filter((color) => color == "darkgreen").length ==
-          colorCode.length
-        ) {
-          finished = true;
-          setTimeout(() => {
-            numpad.querySelectorAll(".numkey").forEach((numkey) => {
-              numkey.classList.remove("flip-in");
-              numkey.classList.add("flip-out");
-            });
-          }, flipDelayTime * 1500);
-          setTimeout(() => {
-            numpad.innerHTML = "";
-            numpad.style.gridTemplateRows = "var(--numpad-size)";
-            for (let numpadKeyIndex = 0; numpadKeyIndex < 2; numpadKeyIndex++) {
-              const numpadKey = document.createElement("button");
-              numpadKey.classList.add("numkey");
-              numpadKey.classList.add("flip-in");
-              numpad.appendChild(numpadKey);
-              switch (numpadKeyIndex) {
-                case 0:
-                  numpadKey.innerHTML = "Restart";
-                  numpadKey.style.gridColumn = "1 / span 2";
-                  numpadKey.classList.add("numkey-text");
-                  numpadKey.addEventListener("click", function () {
-                    location.reload();
-                  });
-                  break;
-                case 1:
-                  numpadKey.innerHTML = "Menu";
-                  numpadKey.style.gridColumn = "3 / span 2";
-                  numpadKey.classList.add("numkey-text");
-                  numpadKey.addEventListener("click", function () {
-                    window.location.href = "index.html";
-                  });
-                  break;
-              }
-            }
-          }, flipDelayTime * 2000);
+
+  let finished = false;
+
+  function updateUserAnswer(key) {
+    const inputRowSquares = Array.from(
+      gridContainer.querySelectorAll(".square")
+    ).slice(-numSquaresPerRow);
+    function modifyInputSquare() {
+      inputRowSquares.forEach((square, i) => {
+        const blankSide = square.querySelector(".blank-side");
+        const digitSide = square.querySelector(".digit-side");
+        if (i < inputEquality.length) {
+          blankSide.innerHTML = inputEquality[i];
+          digitSide.innerHTML = inputEquality[i];
         } else {
-          setTimeout(() => {
-            addRowSquares(gridContainer);
-            lockedPad = false;
-          }, flipDelayTime * 2500);
+          blankSide.innerHTML = "";
+          digitSide.innerHTML = "";
         }
-        inputEquality = "";
-      }
-      break;
-    default:
-      if (allValidCharacters.includes(key) && inputEquality.length < 5) {
-        for (let i = 0; i < keyEquivalents.length; i++) {
-          if (key == keyEquivalents[i][0]) {
-            key = keyEquivalents[i][1];
-          }
+      });
+    }
+    switch (key) {
+      case "Backspace":
+      case "Delete":
+      case "Del":
+        if (inputEquality.length > 0) {
+          inputEquality = inputEquality.slice(0, -1);
         }
-        inputEquality += key;
         modifyInputSquare();
-      }
-      break;
+        break;
+      case "Enter":
+        if (checkValidAnswer(inputEquality) && inputEquality.length == 5) {
+          lockedPad = true;
+          // Handle color Logic
+          const colorCode = [
+            "darkred",
+            "darkred",
+            "darkred",
+            "darkred",
+            "darkred",
+          ];
+          const secondPassInput = [];
+          const secondPassHidden = [];
+          inputEquality.split("").forEach((char, i) => {
+            if (char == hiddenEquality[i]) {
+              colorCode[i] = "darkgreen";
+            } else {
+              secondPassInput.push([i, char]);
+              secondPassHidden.push(hiddenEquality[i]);
+            }
+          });
+          secondPassInput.forEach((input, i) => {
+            if (secondPassHidden.includes(input[1])) {
+              colorCode[input[0]] = "goldenrod";
+              secondPassHidden.splice(secondPassHidden.indexOf(input[1]), 1);
+            }
+          });
+          for (let i = 0; i < numSquaresPerRow; i++) {
+            const square = inputRowSquares[i];
+            const digitSide = square.querySelector(".digit-side");
+            digitSide.style.backgroundColor = colorCode[i];
+            setTimeout(() => {
+              square.classList.add("flipped");
+            }, flipDelayTime * i * 500);
+          }
+          if (
+            colorCode.filter((color) => color == "darkgreen").length ==
+            colorCode.length
+          ) {
+            finished = true;
+            setTimeout(() => {
+              numpad.querySelectorAll(".numkey").forEach((numkey) => {
+                numkey.classList.remove("flip-in");
+                numkey.classList.add("flip-out");
+              });
+            }, flipDelayTime * 1500);
+            setTimeout(() => {
+              numpad.innerHTML = "";
+              numpad.style.gridTemplateRows = "var(--numpad-size)";
+              for (
+                let numpadKeyIndex = 0;
+                numpadKeyIndex < 2;
+                numpadKeyIndex++
+              ) {
+                const numpadKey = document.createElement("button");
+                numpadKey.classList.add("numkey");
+                numpadKey.classList.add("flip-in");
+                numpad.appendChild(numpadKey);
+                switch (numpadKeyIndex) {
+                  case 0:
+                    numpadKey.innerHTML = "Restart";
+                    numpadKey.style.gridColumn = "1 / span 2";
+                    numpadKey.classList.add("numkey-text");
+                    numpadKey.addEventListener("click", function () {
+                      location.reload();
+                    });
+                    break;
+                  case 1:
+                    numpadKey.innerHTML = "Menu";
+                    numpadKey.style.gridColumn = "3 / span 2";
+                    numpadKey.classList.add("numkey-text");
+                    numpadKey.addEventListener("click", function () {
+                      window.location.href = "index.html";
+                    });
+                    break;
+                }
+              }
+            }, flipDelayTime * 2000);
+          } else {
+            setTimeout(() => {
+              addRowSquares(gridContainer);
+              lockedPad = false;
+            }, flipDelayTime * 2500);
+          }
+          inputEquality = "";
+        }
+        break;
+      default:
+        if (allValidCharacters.includes(key) && inputEquality.length < 5) {
+          for (let i = 0; i < keyEquivalents.length; i++) {
+            if (key == keyEquivalents[i][0]) {
+              key = keyEquivalents[i][1];
+            }
+          }
+          inputEquality += key;
+          modifyInputSquare();
+        }
+        break;
+    }
   }
+
+  document.addEventListener("keydown", (event) => {
+    const key = event.key;
+    if (!finished && !lockedPad) {
+      updateUserAnswer(key);
+    }
+  });
+
+  document.addEventListener("click", (event) => {
+    let key = event.target.innerHTML.trim();
+    if (event.target.classList.contains("numkey") && !finished && !lockedPad) {
+      updateUserAnswer(key);
+    }
+  });
 }
 
-document.addEventListener("keydown", (event) => {
-  const key = event.key;
-  if (!finished && !lockedPad) {
-    updateUserAnswer(key);
-  }
-});
-
-document.addEventListener("click", (event) => {
-  let key = event.target.innerHTML.trim();
-  if (event.target.classList.contains("numkey") && !finished && !lockedPad) {
-    updateUserAnswer(key);
-  }
-});
+main();
