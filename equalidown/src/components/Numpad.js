ff891886696b2ef8c5f3472c5a694e49a65217dd
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-05-21 20:45:28 +0700

    Update ui

--- equalidown/src/components/Numpad.js
+++ equalidown/src/components/Numpad.js
@@ -33,36 +33,56 @@ const Numpad = () => {
     "(",
     ")",
     "Backspace",
-    "♼",
+    // "♼",
   ];
 
   const gameCompletedButtonLabels = ["Congratulations!", "Restart", "Menu"];
 
-  const buttons = buttonLabels.map((label, index) => {
-    const isDisabled =
-      equalidownState.buttonState.includes(index) &&
-      equalidownState.numpadNumber.includes(label);
-    const buttonClassNames = `numpad-button ${
-      isDisabled ? "numpad-disabled" : "numpad-active"
-    }`;
+  const numpadClassNames = `numpad ${
+    equalidownState.gameState === "solved" ? "numpad-game-completed" : ""
+  }`;
 
-    return (
-      <button
-        key={index}
-        className={buttonClassNames}
-        ref={index === 0 ? modelButtonRef : null}
-        style={{ height: label === "Enter" ? "auto" : buttonWidth }}
-        onClick={() =>
-          dispatch({ type: "NUMPAD_BUTTON_CLICK", key: [label, index] })
-        }
-        disabled={isDisabled}
-      >
-        {label}
-      </button>
-    );
-  });
+  const buttons =
+    equalidownState.gameState === "solved"
+      ? gameCompletedButtonLabels.map((label, index) => (
+          <button
+            key={index}
+            className="numpad-button numpad-game-completed-button"
+            onClick={() =>
+              dispatch({
+                type: "GAME_COMPLETED_BUTTON_CLICK",
+                key: [label, index],
+              })
+            }
+          >
+            {label}
+          </button>
+        ))
+      : buttonLabels.map((label, index) => {
+          const isDisabled =
+            equalidownState.buttonState.includes(index) &&
+            equalidownState.numpadNumber.includes(label);
+          const buttonClassNames = `numpad-button ${
+            isDisabled ? "numpad-disabled" : "numpad-active"
+          }`;
 
-  return <div className="numpad">{buttons}</div>;
+          return (
+            <button
+              key={index}
+              className={buttonClassNames}
+              ref={index === 0 ? modelButtonRef : null}
+              style={{ height: label === "Enter" ? "auto" : buttonWidth }}
+              onClick={() =>
+                dispatch({ type: "NUMPAD_BUTTON_CLICK", key: [label, index] })
+              }
+              disabled={isDisabled}
+            >
+              {label}
+            </button>
+          );
+        });
+
+  return <div className={numpadClassNames}>{buttons}</div>;
 };
 
 export default Numpad;
